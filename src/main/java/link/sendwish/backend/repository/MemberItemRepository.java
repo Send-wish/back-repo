76b32716ae769e25074af1340f3a6e59ee778dfb
brido4125 <hcs4125@gmail.com>
2023-01-10 23:51:02 +0900

    update:GET 아이템 최신순 적용

--- src/main/java/link/sendwish/backend/repository/MemberItemRepository.java
+++ src/main/java/link/sendwish/backend/repository/MemberItemRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberItemRepository extends JpaRepository<MemberItem, Long> {
-    Optional<List<MemberItem>> findAllByMember(Member member);
+    Optional<List<MemberItem>> findAllByMemberOrderByIdDesc(Member member);
     Optional<List<MemberItem>> findAllByItem(Item item);
 }
