9d3feee5e6fd34d72c154fb8ccb4c2a7bc2484ec
rigyeonghong <77164776+rigyeonghong@users.noreply.github.com>
2023-01-17 14:44:17 +0900

    feat: update ChatRoomResponseDto

--- src/main/java/link/sendwish/backend/dtos/chat/ChatRoomResponseDto.java
+++ src/main/java/link/sendwish/backend/dtos/chat/ChatRoomResponseDto.java
@@ -5,10 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatRoomResponseDto {
     private Long chatRoomId;
+    private String lastMessage;
+    private String sender;
+    private LocalDateTime createAt;
+    private String title;
+    private String defaultImage;
+
 }
